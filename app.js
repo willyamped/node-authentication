f3a10db972e731cabcf54418d57f91868aa24a7b
Willy Angga <ampedwilly@gmail.com>
2023-12-21 13:39:41 +0700

    Change mongodb uri

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://willy:test123@cluster0.wetqo0i.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
