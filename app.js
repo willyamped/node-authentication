4073717789a08628db9ec751b36b491fc9728eb9
Willy Angga <ampedwilly@gmail.com>
2023-12-21 17:23:18 +0700

    Add views for login and signup and add basic cookie

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,3 +24,19 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+// cookies (go to application > cookies)
+app.get('/set-cookies', (req, res) => {
+  //es.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true}); // 1 day
+
+  res.send('you got the cookies!');
+});
+
+app.get('/get-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
+})
