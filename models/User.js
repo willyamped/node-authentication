5c42cc50e82c3991dfef098d48533c27fb45331e
Willy Angga <ampedwilly@gmail.com>
2023-12-21 14:12:40 +0700

    Make signup post save user data

--- models/User.js
+++ models/User.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        rqeuired: true,
+        minLength: 6
+    },
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
